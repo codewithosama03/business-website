811edda989bec0d8b63adc1a0a7a1f059110fe6a
codewithosama03 <osamapathan03@gmail.com>
2025-07-19 14:32:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,176 @@
+'use strict' ;
+
+// adding multiple event listeners on multiple element
+
+    const addEventOnElements = function (elements, eventType, callback) {
+        for (let i = 0, len = elements.length; i < len; i++) {
+          elements[i].addEventListener(eventType, callback);
+        }
+      }
+
+// navbar 
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+// header active when widnows scroll down  to 100px
+
+
+const  header =  document.querySelector("[data-header]");
+
+window.addEventListener("scroll",function(){
+    if(window.scrollY > 100){
+        header.classList.add("active")
+    } else {
+        header.classList.remove("active")
+    }
+})
+
+
+
+
+
+// // slider img
+
+
+// const sliders  = document.querySelectorAll("[data-slider]");
+
+// const initslider = function(currentslider){
+ 
+//   const slideContainer = currentslider.querySelector("[data-slider-container]") 
+//   const sliderPrevbtn = currentslider.querySelector("[data-slider-prev]")
+//   const sliderNextbtn = currentslider.querySelector("[data-slider-next]")
+  
+//   let currentSlidePos =  0;
+
+//   const moveSliderItem = function(){
+//     sldierContainer.style.transform = `translatex(-${sliderContainer.children[currentSlidePos]}
+//     offsetLeft}px)`;
+//   }
+// }
+
+// // previous slide
+
+// const slidePrev = function(){
+//   // const slideEnd = currentSlidePos > sldierContainer.childElementCount - 1;
+  
+//   if(currentSlidePos < 0 ){
+//    currentSlidePos = sldierContainer.childElementCount - 1
+//   } else {
+//     currentSlidePos--;
+//   }
+
+//  moveSliderItem()
+
+// sliderPrevbtn.addEventListener("click", slideprev);
+
+// }
+
+// for(let i = 0 ,  len = sliders.length; i < len; i++){
+//       initslider(sliders[i]); 
+// }
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function(currentSlider) {
+
+  const sldierContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sldierContainer.style.transform = `translateX(-${sldierContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= sldierContainer.childElementCount - 1;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+
+   const slidePrev = function () {
+
+    if (currentSlidePos <= 0) {
+      currentSlidePos = sldierContainer.childElementCount - 1;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = sldierContainer.childElementCount <= 1;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = "none";
+    sliderPrevBtn.style.display = "none";
+  }
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
+
+
+// accordion (abot us dopdowns)
+
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  }
+
+  accordionBtn.addEventListener("click", expandAccordion);
+
+}
+
+for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
+
+
